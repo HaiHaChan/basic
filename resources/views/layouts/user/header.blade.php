4b55970c89668771d717d6930040ed5636dd5caf
HaiHaChan <tran.thi.hai.ha@framgia.com>
2018-08-08 15:07:28 +0700

    init middleware

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -19,7 +19,16 @@
                 <li><a href="#">Laptops</a></li>
                 <li><a href="#">Tablets</a></li>
                 <li><a href="#">Contact Us</a></li>
-                <li><a href="#">404 Page</a></li>
+                <li><a href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                    Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
             </ul>
         </div>
     </nav>
